952631701411520a4141b3a11127a30ee4932ef7
need-lexx <alex.nuzhdin@gmail.com>
2024-05-16 20:32:05 +0300

    Создание таблиц в базе данных
    Создаем базу данных, создали в этой базе 2 таблицы.

--- main.py
+++ main.py
@@ -10,17 +10,60 @@ class DataBase:
 
     def __create_table(self):
         sql = self.connect_db()
-
+        sql["cursor"].execute('''
+                            CREATE TABLE IF NOT EXISTS users (
+                              id                 INTEGER      PRIMARY KEY AUTOINCREMENT,
+                              id_telegram        INTEGER      NOT NULL UNIQUE,
+                              username           TEXT,
+                              first_name         TEXT,
+                              last_name          TEXT,
+                              date_registration  DATE,
+                              access             BOOLEAN      DEFAULT 1                           
+                               )
+                            ''')
+        
+        sql["cursor"].execute('''
+                            CREATE TABLE IF NOT EXISTS messages (
+                              id                 INTEGER      PRIMARY KEY AUTOINCREMENT,
+                              id_user            INTEGER      NOT NULL,
+                              message_id         INTEGER      NOT NULL,
+                              message_text       TEXT         NOT NULL,
+                              data_send          DATE,
+                              status             BOOLEAN      DEFAULT 0  CHECK(status IN (0, 1)), 
+                              FOREIGN KEY (id_user) REFERENCES users(id) 
+                            )         
+                            ''')
         self.close(sql["cursor"], sql["connect"])
 
     def connect_db(self):
         with sqlite3.connect(self.db_name) as connect:
             cursor = connect.cursor()
         return {"cursor": cursor, "connect": connect}
+    
+    def check_user (self, user_id):
+        sql = self.connect_db()
+        sql["cursor"].execute('''
+            SELECT * FROM users WHERE id_telegram =?                               
+        ''', (user_id, ))
+        info_users = sql["cursor"].fetchone()
+        
+        self.close(sql["cursor"], sql["connect"])
+
+        if info_users is None:
+            return {
+                "status": False
+            }
+        return {
+            "status": True
+        } 
+        
 
     def close(self, cursor, connect):
         cursor.close()
         connect.close()
+
+    
+
 
 class TelegramBot(DataBase):
     def __init__(self, db_name, token):
@@ -32,10 +75,15 @@ class TelegramBot(DataBase):
 
         @self.bot.message_handler(commands=['start'])
         def start(message):
-            print(message)
+            text=""
+            if self.check_user(message.from_user.id)["status"]:
+                text += "С возвращением!"
+            else:
+                text += f"Добро пожаловать, {message.from_user.first_name}!"
+            
             self.bot.send_message(
                 message.chat.id,
-                f"Добро пожаловать, {message.from_user.first_name}!"
+                text
             )
 
         @self.bot.message_handler(func=lambda message: True)
